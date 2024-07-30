85ce7973061af1c3ff7364c8ffdfa3044be6109e
bazoka-kaka <tsukikagemaikaka1@gmail.com>
2024-07-30 15:47:21 +0700

    add practice challenge 372

---
+++ artefacts/rev/reverse/ret.c
@@ -0,0 +1,36 @@
+undefined8 main(void)
+
+{
+  int iVar1;
+  long in_FS_OFFSET;
+  char local_68 [48];
+  undefined8 local_38;
+  undefined8 local_30;
+  undefined8 local_28;
+  undefined8 local_20;
+  undefined8 local_18;
+  long local_10;
+  
+  local_10 = *(long *)(in_FS_OFFSET + 0x28);
+  local_38 = 0x7b4654436f636970;
+  local_30 = 0x337633725f666c33;
+  local_28 = 0x75735f676e693572;
+  local_20 = 0x6c75663535656363;
+  local_18 = 0x383035306564315f;
+  printf("Enter the password to unlock this file: ");
+  __isoc99_scanf(&DAT_00102031,local_68);
+  printf("You entered: %s\n",local_68);
+  iVar1 = strcmp(local_68,(char *)&local_38);
+  if (iVar1 == 0) {
+    puts("Password correct, please see flag: picoCTF{3lf_r3v3r5ing_succe55ful_1de05085}");
+    puts((char *)&local_38);
+  }
+  else {
+    puts("Access denied");
+  }
+  if (local_10 != *(long *)(in_FS_OFFSET + 0x28)) {
+                    /* WARNING: Subroutine does not return */
+    __stack_chk_fail();
+  }
+  return 0;
+}
